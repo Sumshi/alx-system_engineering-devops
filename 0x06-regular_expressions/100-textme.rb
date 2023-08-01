9ceaba751dd7f91b303dde4db148b0a7578e1d82
Sumshi <sumayasomow@gmail.com>
2023-08-01 11:05:24 +0000

    trial 4

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-str1 = ARGV[0].scan(/\[from:([^\]]*)\]/).join
-str2 = ARGV[0].scan(/\[to:([^\]]*)\]/).join
-str3 = ARGV[0].scan(/\[flags:([^\]]*)\]/).join
-puts str1 + ',' + str2 + ',' + str3
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
