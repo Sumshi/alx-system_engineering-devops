e25853ac1359a5d4760a32363065e807535a9ad0
Sumshi <sumayasomow@gmail.com>
2023-08-01 15:31:43 +0000

    tests

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+# puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+#!/usr/bin/env ruby
+
+senderPattern = /\[from:(\+?[\w]+)\]/
+receiverPattern = /\[to:(\+?[0-9]+)\]/
+flagsPattern = /\[flags:([012\-:]*)\]/
+
+senders = ARGV[0].scan(senderPattern).flatten
+receivers = ARGV[0].scan(receiverPattern).flatten
+flags = ARGV[0].scan(flagsPattern).flatten
+
+dataArray = senders.zip(receivers, flags)
+
+dataArray.each do |sender, receiver, flag|
+  puts "#{sender},#{receiver},#{flag}"
+end
