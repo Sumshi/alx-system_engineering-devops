b9e55b63a9a39e28ee3f6baecb2fb473614542e1
Sumshi <sumayasomow@gmail.com>
2023-08-01 15:21:25 +0000

    task 3

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
+# t* matches 0 or more occurence of an t
 puts ARGV[0].scan(/hbt*n/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# ^ matches the beginning of a line
+# $ matches position at the end of a string
 puts ARGV[0].scan(/^h.n$/).join
