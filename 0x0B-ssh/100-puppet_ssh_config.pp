afe765c9b57aaf5145a03cdcf540bc2783ba75fa
Sumshi <sumayasomow@gmail.com>
2023-09-23 08:03:12 +0000

    task 5

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,10 @@
+#!/usr/bin/env bash
 # Using Puppet to change our server configuration files
 # Configure SSH to use the private key ~/.ssh/school for authentication
-file { 'configure using private key':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  replace => true,
-}
-
-# Disable password authentication
-file { 'authenticate using a password':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  replace => true,
+  content =>"
+          host*
+          IdentityFile ~/.ssh/school
+          PasswordAuthentication no"
 }
