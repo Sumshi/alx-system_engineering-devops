612859c985804917c5becb5e5957b7f99babd12a
Sumshi <sumayasomow@gmail.com>
2023-10-23 09:52:27 +0000

    fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-''' gather data from an API '''
+
+"""
+Gather data from an API
+for a given employee ID, returns information about his/her
+todo list progress.
+"""
+
+
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    # get employee response [used to get name in line 19]
-    endpoint = 'https://jsonplaceholder.typicode.com'
-    user_res = requests.get(endpoint + '/users/' + argv[1]).json()
 
-    # get total number of tasks [used to get len of all tasks in line 18]
-    todos = requests.get(endpoint + '/todos?userId=' + argv[1]).json()
+if __name__ == "__main__":
+    """Gather data from an API"""
 
-    # get number completed tasks and their titles
-    titles_done = [todo['title'] for todo in todos if todo['completed']]
+    # define the base url of rest api
+    url = "https://jsonplaceholder.typicode.com"
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(user_res['name'], len(titles_done), len(todos)))
+    # send get request to api to get user info in dict format
+    user = requests.get(url + "/users/" + sys.argv[1]).json()
 
-    [print('\t {}'.format(title)) for title in titles_done]
+    # send get request to api to get todo info in dict format
+    todo = requests.get(url + "/todos?userId=" + sys.argv[1]).json()
+
+    # get the number of completed tasks
+    completed_tasks = [task for task in todo if task.get("completed") is True]
+
+    # print the result, employee's name, completed tasks & total no of tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed_tasks), len(todo)))
+
+    # print the title of completed tasks
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
