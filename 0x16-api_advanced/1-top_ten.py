b0164c7c8de469ad2445eb69a60a02524ea4c432
Sumshi <sumayasomow@gmail.com>
2023-11-08 04:30:15 +0000

    top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" queries the Reddit API and prints the titles of the first 10 hot posts"""
+"""Query subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """query a subreddit and retrive top 10 posts"""
-    # Reddit API endpoint for getting subreddit informatiom
+    """Query Reddit and print titles of the first 10 hot posts"""
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
     # Set a custom User-Agent to avoid too many requests error
@@ -15,12 +15,14 @@ def top_ten(subreddit):
     # send a GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # parse JSON response to extract no of subscribers(data field)
-        data = response.json().get('data', {})
-        children = data.get('children', [])
-        for child in children[:10]:
-            print(child.get('data', {}).get('title', ''))
-    else:
-        print(None)
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
